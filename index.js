803dcf24ce04fb03734dabf23f89aa096a33da1d
fridah karimi <fridah.karimi@student.moringaschool.com>
2022-08-24 12:15:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+  });
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left - 1}px`;
+    }
+  });
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+const moveDodgerRight = () => {
+	let left = dodger.style.left
+	left = parseInt(left)
+	dodger.style.left = `${left + 1}px`;
+}
